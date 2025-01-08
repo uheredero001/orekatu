95598b8557613dfe0e9f559365eb10ef4a38b93e
uheredero001 <unai.here@gmail.com>
2025-01-08 17:04:16 +0100

    Orekatu first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Orekatu">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Orekatu
+        </p>
+        <a
+        >
+          Tu centro de psicología
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

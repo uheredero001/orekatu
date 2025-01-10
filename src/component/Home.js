4da7252369dbff6542e808a527245175fbf30b2a
uheredero001 <unai.here@gmail.com>
2025-01-10 14:49:59 +0100

    estilos v1

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <p>Breve descripción de lo que se ofrece</p>
         <p/>
         <p/>
-        <p>Descrubre más...</p>
+        <a id="descubremas" href="whoim">Descrubre más...</a>
         <p/>
         <p/>
         <p>Realiza el test y descrubre si necesitas nuestros servicios</p>
--- src/component/Whoim.js
+++ src/component/Whoim.js
@@ -1,6 +1,18 @@
+import bob from '../images/minionBob.png';
+
 const Whoim = () => {
     return <div>
-        <p>Quien soy</p>
+        <p></p>
+
+        <img src={bob} alt="Bob" width="100" height="140"></img>
+        <br/>
+        <a>Bob</a>
+
+        <p>Una descripción no es otra cosa que explicar mediante palabras, de forma detallada y ordenada, qué es lo que hemos visto o vivido, 
+            ya sean experiencias, paisajes, obras de arte e incluso personas. La finalidad es hacer que la persona que escuche o lea nuestras explicaciones 
+            pueda imaginar el objeto de nuestras descripciones tal cual. En el caso de las personas, además de dar una descripción física, también es importante 
+            como es su carácter. Así pues, para poder hacer un buen trabajo, en unComo te mostramos una serie de notas que te ayudarán a saber cómo hacer la descripción 
+            de una persona.</p>
         
         
         
